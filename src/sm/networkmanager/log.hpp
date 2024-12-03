6217f02a1023e90094cf47e907f413f6d3486758
Mykola Solianko <mykola_solianko@epam.com>
2024-12-03 09:25:54 +0200

    [networkmanager] Implement networkmanager functionality
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/sm/networkmanager/log.hpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2023 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LOG_HPP_
+#define LOG_HPP_
+
+#define LOG_MODULE "networkmanager"
+
+#include "aos/common/tools/logger.hpp"
+
+#endif
