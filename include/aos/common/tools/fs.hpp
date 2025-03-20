d2b8f4308abe873d27b374134598047489cf6fb6
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-03-20 17:34:12 +0200

    [common,tools] Make dir iterator non copyable and movable
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- include/aos/common/tools/fs.hpp
+++ include/aos/common/tools/fs.hpp
@@ -15,6 +15,7 @@
 
 #include "aos/common/tools/config.hpp"
 #include "aos/common/tools/memory.hpp"
+#include "aos/common/tools/noncopyable.hpp"
 #include "aos/common/tools/string.hpp"
 
 namespace aos {
@@ -33,7 +34,7 @@ public:
      * Directory iterator.
      * The iteration order is unspecified, except that each directory entry is visited only once.
      */
-    class DirIterator {
+    class DirIterator : public NonCopyable {
     public:
         /**
          * Directory entry.
@@ -52,6 +53,38 @@ public:
             : mDir(opendir(path.CStr()))
             , mRoot(path)
         {
+        }
+
+        /**
+         * Move constructor.
+         *
+         * @param other iterator to move from.
+         */
+        DirIterator(DirIterator&& other)
+            : mDir(other.mDir)
+            , mEntry(other.mEntry)
+            , mRoot(other.mRoot)
+        {
+            other.mDir = nullptr;
+        }
+
+        /**
+         * Move assignment.
+         *
+         * @param other iterator to move from.
+         * @return DirIterator&.
+         */
+        DirIterator& operator=(DirIterator&& other)
+        {
+            if (this != &other) {
+                mDir   = other.mDir;
+                mEntry = other.mEntry;
+                mRoot  = other.mRoot;
+
+                other.mDir = nullptr;
+            }
+
+            return *this;
         }
 
         /**
