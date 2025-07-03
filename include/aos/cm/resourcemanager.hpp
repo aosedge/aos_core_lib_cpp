22411afc16c20e8a2c94c86ba80b5aad1260d86a
Mykola Kobets <mykola_kobets@epam.com>
2025-07-03 12:51:41 +0300

    cm: add resourcemanager interface
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ include/aos/cm/resourcemanager.hpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_RESOURCEMANAGER_HPP_
+#define AOS_CM_RESOURCEMANAGER_HPP_
+
+#include <aos/common/types.hpp>
+
+namespace aos::cm::resourcemanager {
+
+/** @addtogroup CM ResourceManager
+ *  @{
+ */
+
+/**
+ * Resource manager interface.
+ */
+class ResourceManagerItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~ResourceManagerItf() = default;
+
+    /**
+     * Returns node config by either node id or type.
+     *
+     * @param nodeID node identifier.
+     * @param nodeType node type.
+     * @param[out] nodeConfig param to store node config.
+     * @return Error.
+     */
+    virtual Error GetNodeConfig(const String& nodeID, const String& nodeType, NodeConfig& nodeConfig) = 0;
+};
+
+/** @}*/
+
+} // namespace aos::cm::resourcemanager
+
+#endif
