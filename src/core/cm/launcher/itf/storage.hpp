404a8eab2a5a2e0cbd44e59c65ade391fc5c3598
Mykola Kobets <mykola_kobets@epam.com>
2025-07-03 12:52:04 +0300

    cm: add storage interface
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/core/cm/launcher/itf/storage.hpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_LAUNCHER_STORAGE_HPP_
+#define AOS_CM_LAUNCHER_STORAGE_HPP_
+
+#include <core/common/types/common.hpp>
+
+namespace aos::cm::launcher {
+
+/** @addtogroup CM Storage
+ *  @{
+ */
+
+/**
+ * Persisted instance information.
+ */
+struct InstanceInfo {
+    /**
+     * Instance identifier.
+     */
+    InstanceIdent mInstanceIdent;
+
+    /**
+     * Image identifier.
+     */
+    StaticString<cIDLen> mImageID;
+
+    /**
+     * Instance type.
+     */
+    UpdateItemType mUpdateItemType;
+
+    /**
+     * ID of the node hosting the instance.
+     */
+    StaticString<cIDLen> mNodeID;
+
+    /**
+     * ID of the node that hosted the instance earlier.
+     */
+    StaticString<cIDLen> mPrevNodeID;
+
+    /**
+     * Runtime identifier.
+     */
+    StaticString<cIDLen> mRuntimeID;
+
+    /**
+     * User ID.
+     */
+    uid_t mUID {};
+
+    /**
+     * Timestamp.
+     */
+    Time mTimestamp;
+
+    /**
+     * Cached indicator.
+     */
+    bool mCached {};
+};
+
+/**
+ * Interface for service instance storage.
+ */
+class StorageItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~StorageItf() = default;
+
+    /**
+     * Adds a new instance to the storage.
+     *
+     * @param info instance information.
+     * @return Error.
+     */
+    virtual Error AddInstance(const InstanceInfo& info) = 0;
+
+    /**
+     * Updates an existing instance in the storage.
+     *
+     * @param info updated instance information.
+     * @return Error.
+     */
+    virtual Error UpdateInstance(const InstanceInfo& info) = 0;
+
+    /**
+     * Removes an instance from the storage.
+     *
+     * @param instanceID instance identifier.
+     * @return Error.
+     */
+    virtual Error RemoveInstance(const InstanceIdent& instanceID) = 0;
+
+    /**
+     * Get information about a stored instance.
+     *
+     * @param instanceID instance identifier.
+     * @param[out] info instance info.
+     * @return Error.
+     */
+    virtual Error GetInstance(const InstanceIdent& instanceID, InstanceInfo& info) const = 0;
+
+    /**
+     * Get all stored instances.
+     *
+     * @param[out] instances all stored instances.
+     * @return Error.
+     */
+    virtual Error GetActiveInstances(Array<InstanceInfo>& instances) const = 0;
+};
+
+/** @}*/
+
+} // namespace aos::cm::launcher
+
+#endif
