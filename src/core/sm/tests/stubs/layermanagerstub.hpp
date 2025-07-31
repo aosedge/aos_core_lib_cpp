b7441884c7f8a03b029117b09474722ffba52489
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-07-31 21:53:17 +0300

    sm: tests: change header guards to match Aos core pattern
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/core/sm/tests/stubs/layermanagerstub.hpp
+++ src/core/sm/tests/stubs/layermanagerstub.hpp
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef AOS_LAYERMANAGER_STUB_HPP_
-#define AOS_LAYERMANAGER_STUB_HPP_
+#ifndef AOS_CORE_SM_TESTS_STUBS_LAYERMANAGERSTUB_HPP_
+#define AOS_CORE_SM_TESTS_STUBS_LAYERMANAGERSTUB_HPP_
 
 #include <algorithm>
 #include <mutex>
