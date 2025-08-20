24b83ce6e20d1716ecd8dc7b95f74905be9c946b
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-08-20 10:13:00 +0300

    cm: smcontroller: add UpdateImageProviderItf
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/core/cm/smcontroller/itf/updateimageprovider.hpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_SMCONTROLLER_ITF_UPDATEIMAGEPROVIDER_HPP_
+#define AOS_CORE_CM_SMCONTROLLER_ITF_UPDATEIMAGEPROVIDER_HPP_
+
+#include <core/common/cloudprotocol/common.hpp>
+#include <core/common/tools/error.hpp>
+
+namespace aos::cm::smcontroller {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Update image info.
+ */
+struct UpdateImageInfo {
+    uuid::UUID                mImageID;
+    StaticString<cVersionLen> mVersion;
+    StaticString<cURLLen>     mURL;
+    StaticString<cSHA256Size> mSHA256;
+    size_t                    mSize {};
+
+    /**
+     * Compares update image info.
+     *
+     * @param other object to compare with.
+     * @return bool.
+     */
+    bool operator==(const UpdateImageInfo& other) const
+    {
+        return mImageID == other.mImageID && mVersion == other.mVersion && mURL == other.mURL
+            && mSHA256 == other.mSHA256 && mSize == other.mSize;
+    }
+
+    /**
+     * Compares update image info.
+     *
+     * @param other object to compare with.
+     * @return bool.
+     */
+    bool operator!=(const UpdateImageInfo& other) const { return !operator==(other); }
+};
+
+/**
+ * Provides update item image info.
+ */
+class UpdateImageProviderItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~UpdateImageProviderItf() = default;
+
+    /**
+     * Returns update image info for desired platform.
+     *
+     * @param urn update item URN.
+     * @param platform platform information.
+     * @param[out] info update image info.
+     * @return Error.
+     */
+    virtual Error GetUpdateImageInfo(const String& urn, const PlatformInfo& platform, UpdateImageInfo& info) = 0;
+
+    /**
+     * Returns layer image info.
+     *
+     * @param digest layer digest.
+     * @param info layer image info.
+     * @return Error.
+     */
+    virtual Error GetLayerImageInfo(const String& digest, UpdateImageInfo& info) = 0;
+};
+
+} // namespace aos::cm::smcontroller
+
+#endif
