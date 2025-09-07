606b247a91418e1e8424345f2b73deddf246c7bb
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-09-07 13:13:05 +0300

    cm: smcontroller: add instance status receiver interface
    This interface is used by smcontroller to provide instances statuses received
    from SM's.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/core/cm/smcontroller/itf/instancestatusreceiver.hpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_SMCONTROLLER_ITF_INSTANCESTATUSRECEIVER_HPP_
+#define AOS_CORE_CM_SMCONTROLLER_ITF_INSTANCESTATUSRECEIVER_HPP_
+
+#include <core/common/tools/error.hpp>
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::smcontroller {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Receives instance status updates.
+ */
+class InstanceStatusReceiverItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~InstanceStatusReceiverItf() = default;
+
+    /**
+     * Receives instance status.
+     *
+     * @param status instance status.
+     * @return Error.
+     */
+    virtual Error OnInstanceStatusReceived(const InstanceStatus& status) = 0;
+
+    /**
+     * Receives node instances statuses.
+     *
+     * @param nodeID node ID.
+     * @param statuses instance statuses.
+     * @return Error.
+     */
+    virtual Error OnNodeInstancesStatusesReceived(const String& nodeID, const Array<InstanceStatus>& statuses) = 0;
+};
+
+} // namespace aos::cm::smcontroller
+
+#endif
