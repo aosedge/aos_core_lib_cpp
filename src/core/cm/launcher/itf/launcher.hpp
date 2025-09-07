75512d405c2587781ba416ec9eb95e36e1687f6a
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-09-07 13:14:31 +0300

    cm: launcher: add initial launder interfaces
    Add initial launcher interfaces with documentation.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/core/cm/launcher/itf/launcher.hpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_LAUNCHER_ITF_LAUNCHER_HPP_
+#define AOS_CORE_CM_LAUNCHER_ITF_LAUNCHER_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::launcher {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/*
+ * Instance info.
+ */
+struct InstanceInfo {
+    uuid::UUID        mUpdateItemID;
+    UpdateItemType    mUpdateItemType;
+    uuid::UUID        mSubjectID;
+    uint64_t          mPriority {0};
+    size_t            mNumInstances {0};
+    LabelsStaticArray mLabels;
+
+    /**
+     * Compares instance info.
+     *
+     * @param other instance info to compare.
+     * @return bool.
+     */
+    bool operator==(const InstanceInfo& other) const
+    {
+        return mUpdateItemID == other.mUpdateItemID && mUpdateItemType == other.mUpdateItemType
+            && mSubjectID == other.mSubjectID && mPriority == other.mPriority && mNumInstances == other.mNumInstances
+            && mLabels == other.mLabels;
+    }
+
+    /**
+     * Compares instance info.
+     *
+     * @param other instance info to compare.
+     * @return bool.
+     */
+    bool operator!=(const InstanceInfo& other) const { return !operator==(other); }
+};
+
+/**
+ * Instance launcher interface.
+ */
+class LauncherItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~LauncherItf() = default;
+
+    /**
+     * Returns current statuses of running instances.
+     *
+     * @param statuses instances statuses.
+     * @return Error.
+     */
+    virtual Error GetInstancesStatuses(Array<InstanceStatus>& statuses) = 0;
+
+    /**
+     * Schedules and run instances.
+     *
+     * @param instances instances info.
+     * @param[out] statuses instances statuses.
+     * @return Error.
+     */
+    virtual Error RunInstances(const Array<InstanceInfo>& instances) = 0;
+
+    /**
+     * Rebalances instances.
+     *
+     * @param[out] statuses instances statuses.
+     * @return Error.
+     */
+    virtual Error Rebalance() = 0;
+};
+
+/** @}*/
+
+} // namespace aos::cm::launcher
+
+#endif
