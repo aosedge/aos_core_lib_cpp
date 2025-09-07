83cda65f999b1eab06fca92af1ecda0cbc9a0f97
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-09-07 13:14:31 +0300

    cm: launcher: add initial launder interfaces
    Add initial launcher interfaces with documentation.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/core/cm/launcher/itf/instancerunner.hpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_LAUNCHER_ITF_INSTANCERUNNER_HPP_
+#ifndef AOS_CORE_CM_LAUNCHER_ITF_INSTANCERUNNER_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::launcher {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Instance runner interface.
+ */
+class InstanceRunnerItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~InstanceRunnerItf() = default;
+
+    /**
+     * Updates instances on specified node.
+     *
+     * @param nodeID node ID.
+     * @param stopInstances instance list to stop.
+     * @param startInstances instance list to start.
+     * @return Error.
+     */
+    virtual Error UpdateInstances(const String& nodeID, const Array<aos::InstanceInfo>& stopInstances,
+        const Array<aos::InstanceInfo>& startInstances)
+        = 0;
+}
+
+/** @}*/
+
+} // namespace aos::cm::launcher
+
+#endif // AOS_CORE_CM_LAUNCHER_ITF_STATUSLISTENER_HPP_
---
+++ src/core/cm/launcher/itf/statusnotifier.hpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_LAUNCHER_ITF_STATUSLISTENER_HPP_
+#define AOS_CORE_CM_LAUNCHER_ITF_STATUSLISTENER_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::launcher {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Interface for receiving notification about instance statuses.
+ */
+class StatusListenerItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~StatusListenerItf() = default;
+
+    /**
+     * Notifies about instances statuses change.
+     *
+     * @param statuses instances statuses.
+     */
+    virtual void OnInstancesStatusesChanged(const Array<InstanceStatus>& statuses) = 0;
+};
+
+/**
+ * Interface for notifying about instance statuses.
+ */
+class StatusNotifierItf {
+    /**
+     * Destructor.
+     */
+    virtual ~StatusNotifierItf() = default;
+
+    /**
+     * Subscribes status notifications.
+     *
+     * @param listener status listener.
+     * @return Error.
+     */
+    virtual Error SubscribeListener(StatusListenerItf& listener) = 0;
+
+    /**
+     * Unsubscribes from status notifications.
+     *
+     * @param listener status listener.
+     * @return Error.
+     */
+    virtual Error UnsubscribeListener(StatusListenerItf& listener) = 0;
+};
+
+/** @}*/
+
+} // namespace aos::cm::launcher
+
+#endif
