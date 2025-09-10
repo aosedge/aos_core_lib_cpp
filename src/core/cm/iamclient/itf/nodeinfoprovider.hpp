d32aeb1c8791d4ba1ed3679018ac78d0cc4581b8
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-09-10 19:05:41 +0300

    cm: iamclient: add IAM client node info provider itf
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/core/cm/iamclient/itf/nodeinfoprovider.hpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_COMMON_IAMCLIENT_ITF_NODEINFOPROVIDER_HPP_
+#define AOS_CORE_COMMON_IAMCLIENT_ITF_NODEINFOPROVIDER_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::iamclient {
+/**
+ * Interface for receiving notification about changing node information.
+ */
+class NodeInfoListenerItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~NodeInfoListenerItf() = default;
+
+    /**
+     * Notifies about node info changed.
+     *
+     * @param info node information.
+     */
+    virtual void OnNodeInfoChanged(const NodeInfo& info) = 0;
+};
+
+/**
+ * Node infor provider interface.
+ */
+class NodeInfoProviderItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~NodeInfoProviderItf() = default;
+
+    /**
+     * Returns info for specified node.
+     *
+     * @param nodeID node identifier.
+     * @param[out] nodeInfo result node information.
+     * @return Error.
+     */
+    virtual Error GetNodeInfo(const String& nodeID, NodeInfo& nodeInfo) const = 0;
+
+    /**
+     * Returns ids for all the nodes of the unit.
+     *
+     * @param[out] ids result node identifiers.
+     * @return Error.
+     */
+    virtual Error GetAllNodeIds(Array<StaticString<cNodeIDLen>>& ids) const = 0;
+
+    /**
+     * Subscribes node info notifications.
+     *
+     * @param listener node info listener.
+     * @return Error.
+     */
+    virtual Error SubscribeListener(NodeInfoListenerItf& listener) = 0;
+
+    /**
+     * Unsubscribes from node info notifications.
+     *
+     * @param listener node info listener.
+     * @return Error.
+     */
+    virtual Error UnsubscribeListener(NodeInfoListenerItf& listener) = 0;
+};
+
+} // namespace aos::iamclient
+
+#endif
