401af143185a285ea56f69c76813cdc3354a2729
Mykola Solianko <mykola_solianko@epam.com>
2025-09-15 20:40:13 +0300

    cm: fileserver: add interface
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/fileserver/itf/fileserver.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_FILESERVER_HPP_
+#define AOS_CORE_CM_FILESERVER_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::fileserver {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * File server interface.
+ */
+class FileServerItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~FileServerItf() = default;
+
+    /**
+     * Translates file path URL.
+     *
+     * @param filePath input file path.
+     * @param[out] outURL translated URL.
+     * @return Error.
+     */
+    virtual Error TranslateFilePathURL(const String& filePath, String& outURL) = 0;
+};
+
+} // namespace aos::cm::fileserver
+
+#endif // AOS_CORE_CM_FILESERVER_HPP_
