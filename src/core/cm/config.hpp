8e8a639eb7c361d33b9f70ac2352e89097442641
Mykola Solianko <mykola_solianko@epam.com>
2025-09-15 21:16:49 +0300

    cm: imagemanager: implement imagemanager functionality
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/core/cm/config.hpp
+++ src/core/cm/config.hpp
@@ -7,4 +7,11 @@
 #ifndef AOS_CORE_CM_CONFIG_HPP_
 #define AOS_CORE_CM_CONFIG_HPP_
 
+/**
+ * Number of parallel update items actions.
+ */
+#ifndef AOS_CONFIG_IMAGEMANAGER_NUM_COOPERATE_ACTIONS
+#define AOS_CONFIG_IMAGEMANAGER_NUM_COOPERATE_ACTIONS 5
+#endif
+
 #endif
