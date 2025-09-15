0e0c60ba33ec4786c032df9616b337243699fbea
Mykola Solianko <mykola_solianko@epam.com>
2025-09-15 20:45:22 +0300

    cm: imagemanager: use id instead of urn
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/imagemanager/tests/mocks/statusnotifier.hpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_IMAGEMANAGER_TESTS_MOCKS_STATUSLISTENER_HPP_
+#define AOS_CORE_CM_IMAGEMANAGER_TESTS_MOCKS_STATUSLISTENER_HPP_
+
+#include <gmock/gmock.h>
+
+#include <core/cm/imagemanager/itf/statusnotifier.hpp>
+
+namespace aos::cm::imagemanager {
+
+class MockStatusListener : public StatusListenerItf {
+public:
+    MOCK_METHOD(
+        void, OnImageStatusChanged, (const String& id, const cloudprotocol::UpdateImageStatus& status), (override));
+    MOCK_METHOD(void, OnUpdateItemRemoved, (const String& id), (override));
+};
+
+} // namespace aos::cm::imagemanager
+
+#endif
