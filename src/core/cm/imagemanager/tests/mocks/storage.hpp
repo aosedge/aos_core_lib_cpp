2ef54ec584480377bef9d824fac025e49d8bcb10
Mykola Solianko <mykola_solianko@epam.com>
2025-09-15 20:42:11 +0300

    cm: imagemanager: add storage interface
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/imagemanager/tests/mocks/storage.hpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_IMAGEMANAGER_TESTS_MOCKS_STORAGE_HPP_
+#define AOS_CORE_CM_IMAGEMANAGER_TESTS_MOCKS_STORAGE_HPP_
+
+#include <gmock/gmock.h>
+
+#include <core/cm/imagemanager/itf/storage.hpp>
+
+namespace aos::cm::imagemanager {
+
+class MockStorage : public storage::StorageItf {
+public:
+    MOCK_METHOD(Error, SetItemState, (const String& id, const String& version, storage::ItemState state), (override));
+    MOCK_METHOD(Error, RemoveItem, (const String& id, const String& version), (override));
+    MOCK_METHOD(Error, GetItemsInfo, (Array<storage::ItemInfo> & items), (override));
+    MOCK_METHOD(Error, GetItemVersionsByID, (const String& id, Array<storage::ItemInfo>& items), (override));
+    MOCK_METHOD(Error, AddItem, (const storage::ItemInfo& item), (override));
+};
+
+} // namespace aos::cm::imagemanager
+
+#endif
