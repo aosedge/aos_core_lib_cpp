0cd73e7d9dd01265654562dc1c2abb2c335cfd4a
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-09-19 15:25:54 +0300

    iam: nodeinfoprovider: use string case compare method
    This patch replaces the custom case-insensitive string comparison
    function with String::Compare method.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/iam/nodeinfoprovider/nodeinfoprovider.cpp
+++ src/iam/nodeinfoprovider/nodeinfoprovider.cpp
@@ -7,27 +7,13 @@
 
 #include "aos/iam/nodeinfoprovider.hpp"
 
-static bool CaseInsensitiveEqual(const aos::String& lhs, const aos::String& rhs)
-{
-    if (lhs.Size() != rhs.Size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < lhs.Size(); ++i) {
-        if (tolower(lhs[i]) != tolower(rhs[i])) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 namespace aos::iam::nodeinfoprovider {
 
 bool IsMainNode(const NodeInfo& nodeInfo)
 {
-    return nodeInfo.mAttrs.FindIf([](const auto& attr) { return CaseInsensitiveEqual(attr.mName, cAttrMainNode); })
-        != nodeInfo.mAttrs.end();
+    return nodeInfo.mAttrs.FindIf([](const auto& attr) {
+        return !attr.mName.Compare(cAttrMainNode, String::CaseSensitivity::CaseInsensitive);
+    }) != nodeInfo.mAttrs.end();
 }
 
 } // namespace aos::iam::nodeinfoprovider
