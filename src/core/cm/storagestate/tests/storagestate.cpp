f44898f91a7ff071d255a60d63d212e7a8f97197
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-09-19 16:08:34 +0300

    cm: storagestate: move storagestate to itf directory
    This patch moves the storagestate interface to the itf directory
    and updates the interface.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/storagestate/tests/storagestate.cpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <core/cm/storagestate/itf/sender.hpp>
+#include <core/cm/storagestate/itf/storage.hpp>
+#include <core/cm/storagestate/itf/storagestate.hpp>
