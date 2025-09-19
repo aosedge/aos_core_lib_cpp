560081a425079e96b4e1e90bf989a5e6027175db
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-09-19 16:06:39 +0300

    cm: storagestate: add sender interface
    This patch adds an interface for sending storage state messages.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/storagestate/itf/sender.hpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_STORAGESTATE_ITF_SENDER_HPP_
+#define AOS_CORE_CM_STORAGESTATE_ITF_SENDER_HPP_
+
+#include <core/common/crypto/crypto.hpp>
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::storagestate {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Sender interface.
+ */
+class SenderItf {
+public:
+    /**
+     * Sends state request for the instance.
+     *
+     * @param instanceIdent instance ident.
+     * @param isDefault true if default state is requested.
+     * @return Error.
+     */
+    virtual Error SendStateRequest(const InstanceIdent& instanceIdent, bool isDefault) = 0;
+
+    /**
+     * Sends instance's new state.
+     *
+     * @param instanceIdent instance ident.
+     * @param state new state.
+     * @param checksum state checksum.
+     * @return Error.
+     */
+    virtual Error SendNewState(const InstanceIdent& instanceIdent, const String& state, const String& checksum) = 0;
+
+    /**
+     * Destructor.
+     */
+    virtual ~SenderItf() = default;
+};
+
+/** @}*/
+
+} // namespace aos::cm::storagestate
+
+#endif
