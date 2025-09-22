76bb1a49085a233e9f2d51fc651f1f7aa442ae9f
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-09-22 16:02:43 +0300

    cm: storagestate: add config structure
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/storagestate/config.hpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_STORAGESTATE_CONFIG_HPP_
+#define AOS_CORE_CM_STORAGESTATE_CONFIG_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::storagestate {
+
+/**
+ * Storage state configuration.
+ */
+struct Config {
+    StaticString<cFilePathLen> mStorageDir;
+    StaticString<cFilePathLen> mStateDir;
+};
+
+} // namespace aos::cm::storagestate
+
+#endif
