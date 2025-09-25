ae466a78669d76358344631215928dd805813e3f
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-09-25 11:31:23 +0300

    cm: storagestate: add state interface
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/storagestate/itf/state.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_STORAGESTATE_ITF_STATE_HPP_
+#define AOS_CORE_CM_STORAGESTATE_ITF_STATE_HPP_
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::storagestate {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Interface to manage state.
+ */
+class StateItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~StateItf() = default;
+
+    /**
+     * Updates storage state with new state.
+     *
+     * @param instanceIdent instance ident.
+     * @param state state content.
+     * @param checksum state checksum.
+     * @return Error.
+     */
+    virtual Error UpdateState(const InstanceIdent& instanceIdent, const String& state, const String& checksum) = 0;
+
+    /**
+     * Accepts state from storage.
+     *
+     * @param instanceIdent instance ident.
+     * @param checksum state checksum.
+     * @param result state result.
+     * @param reason reason of the result.
+     * @return Error.
+     */
+    virtual Error AcceptState(
+        const InstanceIdent& instanceIdent, const String& checksum, StateResult result, const String& reason)
+        = 0;
+};
+
+/** @}*/
+
+} // namespace aos::cm::storagestate
+
+#endif
