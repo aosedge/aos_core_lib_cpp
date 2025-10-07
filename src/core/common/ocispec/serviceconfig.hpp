91649656bd47c9fbe5db327ef1d2249b963d7f9b
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-10-08 00:35:13 +0300

    common: types: move permissions structs to types permissions.hpp
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/core/common/ocispec/serviceconfig.hpp
+++ src/core/common/ocispec/serviceconfig.hpp
@@ -9,7 +9,7 @@
 
 #include <core/common/tools/map.hpp>
 #include <core/common/tools/optional.hpp>
-#include <core/common/types/common.hpp>
+#include <core/common/types/permissions.hpp>
 
 #include "common.hpp"
 
