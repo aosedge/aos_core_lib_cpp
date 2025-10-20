d018b73587baea081a280ce3a1be21ccf72bcfd3
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-10-20 21:37:23 +0300

    cm: updatemanager: add updatemanager interface
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/core/cm/updatemanager/tests/updatemanager.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <core/cm/updatemanager/itf/sender.hpp>
+#include <core/cm/updatemanager/itf/updatemanager.hpp>
