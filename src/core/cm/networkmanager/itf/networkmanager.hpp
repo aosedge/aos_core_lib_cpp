0d545a22f28c7ed7c0a0d740e3a36a432f5bae00
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-10-20 21:23:57 +0300

    cm: networkmanager: add NodeNetworkItf to update SM networks
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/core/cm/networkmanager/itf/networkmanager.hpp
+++ src/core/cm/networkmanager/itf/networkmanager.hpp
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef AOS_CORE_CM_NETWORKMANAGER_HPP_
-#define AOS_CORE_CM_NETWORKMANAGER_HPP_
+#ifndef AOS_CORE_CM_NETWORKMANAGER_ITF_NETWORKMANAGER_HPP_
+#define AOS_CORE_CM_NETWORKMANAGER_ITF_NETWORKMANAGER_HPP_
 
 #include <core/cm/config.hpp>
 #include <core/common/types/network.hpp>
