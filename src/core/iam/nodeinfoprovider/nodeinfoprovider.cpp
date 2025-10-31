c772c5a26bc5fee0bbc34dfd50e16c0f506801a2
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-10-31 13:42:38 +0200

    iam: nodeinfoprovider: add helper to check if node contains component
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/core/iam/nodeinfoprovider/nodeinfoprovider.cpp
+++ src/core/iam/nodeinfoprovider/nodeinfoprovider.cpp
@@ -9,11 +9,43 @@
 
 namespace aos::iam::nodeinfoprovider {
 
+/***********************************************************************************************************************
+ * Consts
+ **********************************************************************************************************************/
+
+constexpr auto cNodeComponentStrLen  = 8;
+constexpr auto cMaxNumNodeComponents = static_cast<size_t>(CoreComponentEnum::eNumComponents);
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
 bool IsMainNode(const NodeInfoObsolete& nodeInfo)
 {
     return nodeInfo.mAttrs.FindIf([](const auto& attr) {
         return !attr.mName.Compare(cAttrMainNode, String::CaseSensitivity::CaseInsensitive);
     }) != nodeInfo.mAttrs.end();
+}
+
+bool ContainsComponent(const NodeInfo& nodeInfo, const CoreComponent& component)
+{
+    auto it = nodeInfo.mAttrs.FindIf([](const NodeAttribute& attr) {
+        return !attr.mName.Compare(cAttrAosComponents, String::CaseSensitivity::CaseInsensitive);
+    });
+
+    if (it == nodeInfo.mAttrs.end()) {
+        return false;
+    }
+
+    StaticArray<StaticString<cNodeComponentStrLen>, cMaxNumNodeComponents> components;
+
+    if (auto err = it->mValue.Split(components, ','); !err.IsNone()) {
+        return false;
+    }
+
+    return components.FindIf([&component](const String& compStr) {
+        return !compStr.Compare(component.ToString(), String::CaseSensitivity::CaseInsensitive);
+    }) != components.end();
 }
 
 } // namespace aos::iam::nodeinfoprovider
