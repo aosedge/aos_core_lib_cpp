e5a8e797080aae5a685d97e1517eef95f8d6acca
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-11-03 14:09:58 +0200

    cm: communication: update communication interface
    This patch moves the communication interface to itf folder,
    and updates it to inherit from specific sender interfaces.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/cm/communication/itf/communication.hpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CM_COMMUNICATION_ITF_COMMUNICATION_HPP_
+#define AOS_CORE_CM_COMMUNICATION_ITF_COMMUNICATION_HPP_
+
+#include <core/cm/alerts/itf/sender.hpp>
+#include <core/cm/launcher/itf/sender.hpp>
+#include <core/cm/monitoring/itf/sender.hpp>
+#include <core/cm/smcontroller/itf/sender.hpp>
+#include <core/cm/storagestate/itf/sender.hpp>
+#include <core/cm/updatemanager/itf/sender.hpp>
+#include <core/common/cloudconnection/itf/cloudconnection.hpp>
+
+namespace aos::cm::communication {
+
+/** @addtogroup cm Communication Manager
+ *  @{
+ */
+
+/**
+ * Communication interface.
+ */
+class CommunicationItf : public alerts::SenderItf,
+                         public launcher::SenderItf,
+                         public monitoring::SenderItf,
+                         public smcontroller::SenderItf,
+                         public storagestate::SenderItf,
+                         public updatemanager::SenderItf,
+                         public cloudconnection::CloudConnectionItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~CommunicationItf() override = default;
+};
+
+/** @}*/
+
+} // namespace aos::cm::communication
+
+#endif
