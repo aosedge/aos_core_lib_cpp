c0dd24c14acde310990a55ed8baac4af0500c304
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-11-13 12:46:21 +0200

    sm: launcher: add launcher design document and base interfaces
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/core/sm/launcher/itf/storage.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_SM_LAUNCHER_ITF_STORAGE_HPP_
+#define AOS_CORE_SM_LAUNCHER_ITF_STORAGE_HPP_
+
+#include <core/common/monitoring/itf/instanceparamsprovider.hpp>
+#include <core/common/types/instance.hpp>
+
+namespace aos::sm::launcher {
+
+/**
+ * Launcher storage interface.
+ */
+class StorageItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~StorageItf() = default;
+
+    /**
+     * Gets all instances infos from storage.
+     *
+     * @param[out] infos array to store instance infos.
+     * @return Error.
+     */
+    virtual Error GetAllInstancesInfos(Array<InstanceInfo>& infos) = 0;
+
+    /**
+     * Stores instance info to storage.
+     *
+     * @param info instance info to store.
+     * @return Error.
+     */
+    virtual Error AddInstanceInfo(const InstanceInfo& info) = 0;
+
+    /**
+     * Deletes instance info from storage.
+     *
+     * @param instanceIdent instance ident.
+     * @return Error.
+     */
+    virtual Error RemoveInstanceInfo(const InstanceIdent& ident) = 0;
+};
+
+/** @}*/
+
+} // namespace aos::sm::launcher
+
+#endif
