b3540ea1f285694b293352007f9b4d3f68aec994
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-11-19 11:20:58 +0200

    common: nodeconfig: add node config provider itf
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/core/common/nodeconfig/tests/nodeconfig.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <core/common/nodeconfig/itf/jsonprovider.hpp>
+#include <core/common/nodeconfig/itf/nodeconfigprovider.hpp>
