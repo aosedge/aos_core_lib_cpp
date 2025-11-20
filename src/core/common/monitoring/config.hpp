446e9b2c13c697e1e9741b4f16956683a763e647
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-11-20 20:37:00 +0200

    common: monitoring: add config header
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/core/common/monitoring/config.hpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_COMMON_MONITORING_CONFIG_HPP_
+#define AOS_CORE_COMMON_MONITORING_CONFIG_HPP_
+
+#include <core/common/config.hpp>
+#include <core/common/tools/memory.hpp>
+
+namespace aos::monitoring {
+
+/**
+ * Monitor config.
+ */
+struct Config {
+    Duration mPollPeriod    = AOS_CONFIG_MONITORING_POLL_PERIOD_SEC * Time::cSeconds;
+    Duration mAverageWindow = AOS_CONFIG_MONITORING_AVERAGE_WINDOW_SEC * Time::cSeconds;
+};
+
+} // namespace aos::monitoring
+
+#endif
