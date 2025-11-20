52e64a7a3ff2e76aaae0b73eda0bd29e495f0ab3
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-11-20 20:41:19 +0200

    common: monitoring: use alert point rules for instances
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/core/common/monitoring/itf/instanceparamsprovider.hpp
+++ src/core/common/monitoring/itf/instanceparamsprovider.hpp
@@ -20,6 +20,63 @@ struct PartitionParam {
 };
 
 /**
+ * Partition alert rule.
+ */
+struct PartitionAlertRule : public AlertRulePoints {
+    StaticString<cPartitionNameLen> mName;
+
+    /**
+     * Compares partition alert rule.
+     *
+     * @param rhs partition alert rule to compare.
+     * @return bool.
+     */
+    bool operator==(const PartitionAlertRule& rhs) const
+    {
+        return mName == rhs.mName && static_cast<const AlertRulePoints&>(*this) == rhs;
+    }
+
+    /**
+     * Compares partition alert rule.
+     *
+     * @param rhs partition alert rule to compare.
+     * @return bool.
+     */
+    bool operator!=(const PartitionAlertRule& rhs) const { return !operator==(rhs); }
+};
+
+/**
+ * Alert rules.
+ */
+struct AlertRules {
+    Optional<AlertRulePoints>                          mRAM;
+    Optional<AlertRulePoints>                          mCPU;
+    StaticArray<PartitionAlertRule, cMaxNumPartitions> mPartitions;
+    Optional<AlertRulePoints>                          mDownload;
+    Optional<AlertRulePoints>                          mUpload;
+
+    /**
+     * Compares alert rules.
+     *
+     * @param rhs alert rules to compare.
+     * @return bool.
+     */
+    bool operator==(const AlertRules& rhs) const
+    {
+        return mRAM == rhs.mRAM && mCPU == rhs.mCPU && mPartitions == rhs.mPartitions && mDownload == rhs.mDownload
+            && mUpload == rhs.mUpload;
+    }
+
+    /**
+     * Compares alert rules.
+     *
+     * @param rhs alert rules to compare.
+     * @return bool.
+     */
+    bool operator!=(const AlertRules& rhs) const { return !operator==(rhs); }
+};
+
+/**
  * Instance monitoring parameters.
  */
 struct InstanceParams {
