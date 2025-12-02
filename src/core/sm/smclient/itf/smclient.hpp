3fa844a06070a645091081f20717a2c184a4e7cd
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-02 12:20:50 +0200

    sm: smclient: add initial smclient interfaces
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/core/sm/smclient/itf/smclient.hpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef AOS_CORE_SM_SMCLIENT_ITF_SMCLIENT_HPP_
+#define AOS_CORE_SM_SMCLIENT_ITF_SMCLIENT_HPP_
+
+#include <core/common/alerts/itf/sender.hpp>
+#include <core/common/blobinfoprovider/itf/blobinfoprovider.hpp>
+#include <core/common/cloudconnection/itf/cloudconnection.hpp>
+#include <core/common/logging/itf/sender.hpp>
+#include <core/common/monitoring/itf/sender.hpp>
+#include <core/sm/launcher/itf/sender.hpp>
+
+namespace aos::sm::smclient {
+
+/** @addtogroup sm Service Manager
+ *  @{
+ */
+
+/**
+ * SM client.
+ */
+class SMClientItf : public alerts::SenderItf,
+                    public monitoring::SenderItf,
+                    public logging::SenderItf,
+                    public launcher::SenderItf,
+                    public blobinfoprovider::ProviderItf,
+                    public cloudconnection::CloudConnectionItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~SMClientItf() = default;
+};
+
+/** @}*/
+
+} // namespace aos::sm::smclient
+
+#endif
