daa2997777b65dae5800325ed717a92935aa2a30
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-12-04 13:01:29 +0200

    sm: tests: mocks: add UT to mock structs and fix build
    The mocks target is not compiled, so the mock structs were not
    checked for correctness. This commit adds a simple test to verify
    the mocks and also fixes the build issues in mock headers.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/core/sm/tests/mocks/launchermock.hpp
+++ src/core/sm/tests/mocks/launchermock.hpp
@@ -9,7 +9,7 @@
 
 #include <gmock/gmock.h>
 
-#include <core/sm/launcher/launcher.hpp>
+#include <core/sm/launcher/itf/launcher.hpp>
 
 namespace aos::sm::launcher {
 
@@ -18,36 +18,8 @@ namespace aos::sm::launcher {
  */
 class LauncherMock : public LauncherItf {
 public:
-    MOCK_METHOD(Error, RunInstances,
-        (const Array<ServiceInfo>&, const Array<LayerInfo>&, const Array<InstanceInfo>&, bool), (override));
-    MOCK_METHOD(Error, GetCurrentRunStatus, (Array<InstanceStatus>&), (const override));
-    MOCK_METHOD(Error, OverrideEnvVars, (const Array<EnvVarsInstanceInfo>&, Array<EnvVarsInstanceStatus>&), (override));
-};
-
-/**
- * Instance status receiver mock.
- */
-class InstanceStatusReceiverMock : public InstanceStatusReceiverItf {
-public:
-    MOCK_METHOD(Error, InstancesRunStatus, (const Array<InstanceStatus>&), (override));
-    MOCK_METHOD(Error, InstancesUpdateStatus, (const Array<InstanceStatus>&), (override));
-};
-
-/**
- * Runtime mock.
- */
-class RuntimeMock : public RuntimeItf {
-public:
-    MOCK_METHOD(Error, CreateHostFSWhiteouts, (const String&, const Array<StaticString<cFilePathLen>>&), (override));
-    MOCK_METHOD(Error, CreateMountPoints, (const String&, const Array<Mount>&), (override));
-    MOCK_METHOD(Error, MountServiceRootFS, (const String&, const Array<StaticString<cFilePathLen>>&), (override));
-    MOCK_METHOD(Error, UmountServiceRootFS, (const String&), (override));
-    MOCK_METHOD(Error, PrepareServiceStorage, (const String& path, uint32_t uid, uint32_t gid), (override));
-    MOCK_METHOD(Error, PrepareServiceState, (const String& path, uint32_t uid, uint32_t gid), (override));
-    MOCK_METHOD(Error, PrepareNetworkDir, (const String& path), (override));
-    MOCK_METHOD(RetWithError<StaticString<cFilePathLen>>, GetAbsPath, (const String& path), (override));
-    MOCK_METHOD(RetWithError<uint32_t>, GetGIDByName, (const String& groupName), (override));
-    MOCK_METHOD(Error, PopulateHostDevices, (const String& devicePath, Array<oci::LinuxDevice>& devices), (override));
+    MOCK_METHOD(Error, UpdateInstances,
+        (const Array<InstanceIdent>& stopInstances, const Array<InstanceInfo>& startInstances), (override));
 };
 
 } // namespace aos::sm::launcher
---
+++ src/core/sm/tests/mocks/tests/mocks.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <core/sm/tests/mocks/cnimock.hpp>
+#include <core/sm/tests/mocks/launchermock.hpp>
+#include <core/sm/tests/mocks/networkmanagermock.hpp>
+#include <core/sm/tests/mocks/resourcemanagermock.hpp>
+#include <core/sm/tests/mocks/runnermock.hpp>
+#include <core/sm/tests/mocks/storagemock.hpp>
