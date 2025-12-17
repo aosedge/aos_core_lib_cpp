f49fd3ff76cdac67a7c2ffcbbe9beb92d1290849
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-12-17 19:50:06 +0200

    cm: imagemanager: fix missed include
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/core/cm/imagemanager/config.hpp
+++ src/core/cm/imagemanager/config.hpp
@@ -7,6 +7,7 @@
 #ifndef AOS_CORE_CM_IMAGEMANAGER_CONFIG_HPP_
 #define AOS_CORE_CM_IMAGEMANAGER_CONFIG_HPP_
 
+#include <core/common/consts.hpp>
 #include <core/common/tools/string.hpp>
 
 namespace aos::cm::imagemanager {
