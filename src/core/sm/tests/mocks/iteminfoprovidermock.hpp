b9ccb6205a5583fe0fa7a5078f35d01bf3626a8b
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-30 11:32:53 +0200

    sm: tests: mocks: add item info provider mock
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/core/sm/tests/mocks/iteminfoprovidermock.hpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_SM_TESTS_MOCKS_ITEMINFOPROVIDER_HPP_
+#define AOS_CORE_SM_TESTS_MOCKS_ITEMINFOPROVIDER_HPP_
+
+#include <gmock/gmock.h>
+
+#include <core/sm/imagemanager/itf/iteminfoprovider.hpp>
+
+namespace aos::sm::imagemanager {
+
+/**
+ * Item info provider mock.
+ */
+class ItemInfoProviderMock : public ItemInfoProviderItf {
+public:
+    MOCK_METHOD(Error, GetBlobPath, (const String&, String&), (const, override));
+    MOCK_METHOD(Error, GetLayerPath, (const String&, String&), (const, override));
+};
+
+} // namespace aos::sm::imagemanager
+
+#endif
