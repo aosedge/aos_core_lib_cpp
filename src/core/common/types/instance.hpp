b86c7704a42ef92ec1c6a81d347502e34831aa15
Mykola Kobets <mykola_kobets@epam.com>
2026-03-05 14:27:04 +0200

    common: fix InstanceInfoData::operator==
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/core/common/types/instance.hpp
+++ src/core/common/types/instance.hpp
@@ -44,7 +44,7 @@ struct InstanceInfoData {
     {
         return mManifestDigest == rhs.mManifestDigest && mRuntimeID == rhs.mRuntimeID && mOwnerID == rhs.mOwnerID
             && mSubjectType == rhs.mSubjectType && mGID == rhs.mGID && mUID == rhs.mUID && mPriority == rhs.mPriority
-            && mStoragePath == rhs.mStoragePath && mStatePath == rhs.mStatePath
+            && mStoragePath == rhs.mStoragePath && mStatePath == rhs.mStatePath && mEnvVars == rhs.mEnvVars
             && mNetworkParameters == rhs.mNetworkParameters && mMonitoringParams == rhs.mMonitoringParams;
     }
 
