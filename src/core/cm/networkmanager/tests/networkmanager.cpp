6ec4b88f1a9b4da86b648fe4ab052402a5e9676c
Mykola Solianko <mykola_solianko@epam.com>
2026-03-18 15:38:28 +0200

    cm: remove networkmanager module, update launcher and smcontroller
    - Remove CM networkmanager module (moved to SM-driven model)
    - Update CM launcher to remove network management
    - Update smcontroller to use NetworkProviderItf
    
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/core/cm/networkmanager/tests/networkmanager.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 2025 EPAM Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <core/cm/networkmanager/itf/networkmanager.hpp>
-#include <core/cm/networkmanager/itf/nodenetwork.hpp>
