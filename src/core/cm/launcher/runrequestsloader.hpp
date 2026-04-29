e14186b8ab0b10e9f411e2ede9b7560a2896ef4a
Mykola Kobets <mykola_kobets@epam.com>
2026-04-29 03:02:11 +0300

    cm: launcher: consider runtime info while generating component instances
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/core/cm/launcher/runrequestsloader.hpp
+++ src/core/cm/launcher/runrequestsloader.hpp
@@ -23,8 +23,9 @@ public:
      *
      * @param storage storage interface.
      * @param instanceManager instance manager.
+     * @param imageInfoProvider image info provider.
      */
-    void Init(StorageItf& storage, InstanceManager& instanceManager);
+    void Init(StorageItf& storage, InstanceManager& instanceManager, ImageInfoProvider& imageInfoProvider);
 
     /**
      * Saves run requests to internal buffer and to storage.
@@ -51,9 +52,26 @@ public:
     void CreateInstances(const Array<Node>& nodes, Array<SharedPtr<Instance>>& instances);
 
 private:
-    StorageItf*                                       mStorage {};
-    InstanceManager*                                  mInstanceManager {};
+    using CombinedRuntimesArray
+        = StaticArray<StaticString<cRuntimeTypeLen>, oci::cMaxNumManifests * oci::cMaxNumRunners>;
+
+    static constexpr auto cCombinedRuntimesSize = sizeof(CombinedRuntimesArray);
+    static constexpr auto cAllocatorSize = sizeof(oci::ImageIndex) + cCombinedRuntimesSize + sizeof(oci::ItemConfig);
+
+    Error GenerateInstances(
+        const RunInstanceRequest& request, const Array<Node>& nodes, Array<SharedPtr<Instance>>& instances);
+    Error CombinedRuntimes(
+        const oci::ImageIndex& imageIndex, CombinedRuntimesArray& combinedRuntimes, oci::ItemConfig& itemConfig);
+    void CreateGeneratedInstances(const RunInstanceRequest& request, const Array<Node>& nodes,
+        const CombinedRuntimesArray& combinedRuntimes, Array<SharedPtr<Instance>>& instances);
+
+    StorageItf*        mStorage {};
+    InstanceManager*   mInstanceManager {};
+    ImageInfoProvider* mImageInfoProvider {};
+
     StaticArray<RunInstanceRequest, cMaxNumInstances> mRunRequests;
+
+    StaticAllocator<cAllocatorSize> mAllocator;
 };
 
 } // namespace aos::cm::launcher
