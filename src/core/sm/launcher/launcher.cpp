1404ddc24765d117ccef60e2e69108e77fe44011
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-07-13 12:33:18 +0300

    sm: launcher: remove instance state checks in RemoveInstances
    RemoveInstances skipped releasing/removing instance data whose state
    was not eInactive. By the time RemoveInstances runs, matching
    instances have already been stopped via the launch pool, so the
    check only caused otherwise removable instances to be silently left
    in mInstances. Drop the redundant state checks.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>

--- src/core/sm/launcher/launcher.cpp
+++ src/core/sm/launcher/launcher.cpp
@@ -1276,13 +1276,6 @@ void Launcher::RemoveInstances(const Array<InstanceIdent>& instances)
             continue;
         }
 
-        if (instanceData->mStatus.mState != InstanceStateEnum::eInactive) {
-            LOG_ERR() << "Instance is not inactive, skip removing" << Log::Field("instance", instanceIdent)
-                      << Log::Field("state", instanceData->mStatus.mState);
-
-            continue;
-        }
-
         if (auto err = mLaunchPool.AddTask([this, instanceData](void*) {
                 if (auto err = ReleaseInstance(*instanceData); !err.IsNone()) {
                     LOG_ERR() << "Failed to remove instance" << Log::Field("instance", instanceData->mInfo)
@@ -1309,18 +1302,7 @@ void Launcher::RemoveInstances(const Array<InstanceIdent>& instances)
         LOG_DBG() << "Remove instance data" << Log::Field("instance", instanceIdent);
 
         mInstances.RemoveIf([this, &instanceIdent](const auto& instance) {
-            if (static_cast<const InstanceIdent&>(instance.mInfo) != instanceIdent) {
-                return false;
-            }
-
-            if (instance.mStatus.mState != InstanceStateEnum::eInactive) {
-                LOG_ERR() << "Instance is not inactive, skip removing" << Log::Field("instance", instanceIdent)
-                          << Log::Field("state", instance.mStatus.mState);
-
-                return false;
-            }
-
-            return true;
+            return static_cast<const InstanceIdent&>(instance.mInfo) == instanceIdent;
         });
     }
 }
